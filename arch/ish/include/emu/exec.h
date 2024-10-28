3c06c4faa67cf4db6144e7c686ab5b4e9539ec82
Theodore Dubois <tblodt@icloud.com>
2024-10-28 09:42:38 -0700

    Move emulator files around a little bit
    Make new subdirectory for emulators in preparation for there being
    multiple.

--- arch/ish/include/emu/exec.h
+++ arch/ish/include/emu/exec.h
@@ -1,11 +1,15 @@
 #ifndef __ISH_EXEC_H
 #define __ISH_EXEC_H
 
+struct emu;
+struct emu_mm;
+
 extern int emu_run_to_interrupt(struct emu *emu, struct pt_regs *regs);
 extern void emu_poke_cpu(int cpu);
 
 extern void emu_mmu_init(struct emu_mm *mm);
 extern void emu_mmu_destroy(struct emu_mm *mm);
 extern void emu_switch_mm(struct emu *emu, struct emu_mm *mm);
+extern void emu_flush_tlb_local(struct emu_mm *mm, unsigned long start, unsigned long end);
 
 #endif
